53da798e15bf94908156eed02d519f4d753dd5fc
Reimond Pena <elderreimondpena@gmail.com>
2021-05-26 18:14:30 -0400

    Cambios sobre bandas terminadas

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,21 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Bon Jovi'));
+bands.addBand(new Band('Heroes del silencio'));
+bands.addBand(new Band('Metalica'));
+
+console.log(bands);
 
 //Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBand());
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
@@ -12,4 +25,21 @@ io.on('connection', client => {
         console.log('Mensaje!!!', payload);
         io.emit('mensaje', { admin: 'Nuevo mensaje' });
     });
+
+    client.on('vote-band', (payload) => {
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBand());
+    });
+
+    client.on('add-band', (payload) => {
+        const _newBand = new Band(payload.name);
+        bands.addBand(_newBand);
+        io.emit('active-bands', bands.getBand());
+    });
+
+    client.on('delete-band', (payload) => {
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBand());
+    });
 });
+
